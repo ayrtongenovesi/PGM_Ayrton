3ed62420a691669a9191cad7e333d64938ef1eea
ayrtongenovesi <genovesiayrton5@gmail.com>
2025-06-11 11:39:40 -0300

    Improve login text and future date limit

--- frontEnd/src/app/components/orden-trabajo/orden-trabajo.component.ts
+++ frontEnd/src/app/components/orden-trabajo/orden-trabajo.component.ts
@@ -22,7 +22,7 @@ import { Tarea } from '../../interfaces/tarea';
 })
 
 export class OrdenTrabajoComponent {
-  maxFecha: string = new Date().toISOString().split('T')[0];
+  minFecha: string = new Date().toISOString().split('T')[0];
   selectedFecha: string = '';
   selectedEdificio: string = '';
   selectedSector: string = '';
